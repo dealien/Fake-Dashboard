397e05c81417f7cf8027ccb4fce88213c6d1e7fd
dealien <dragonofawsome@gmail.com>
2017-01-06 15:25:00 -0500

    Updated auto-compiler

--- Test Website.bat
+++ Test Website.bat
@@ -1,1 +1,4 @@
+rem Requires Gulp to be installed 
+rem To install Gulp from Node Package Manager, run the following from the command line: npm install -g gulp-cli
+rem Then just run this batch file to compile the site. It will automatcally open once it finishes compiling. 
 gulp
